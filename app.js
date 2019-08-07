756370a46f3c84f28678fd6a7cc93abda93d0f31
karslio <karslio@users.noreply.github.com>
2019-08-07 17:45:03 +0200

    filter ok

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-// Make the project great again with jQuery ..
+$("button").click(function(e) {
+  e.preventDefault();
+  let newAdd = $("#add").val();
+  let newSpan1 = "<span>" + newAdd + "</span>";
+  let newSpan2 = '<span class="delete">delete</span>';
+  let newList = $("<li>" + newSpan1 + newSpan2 + "</li>");
+  $("ul").append(newList);
+  $("#add").val("");
+});
+
+$(".delete").on("click", function() {
+  $(this)
+    .parent("li")
+    .remove();
+});
+
+$("#search-topics").on("keyup", function() {
+  let value = $("input").val().toLowerCase();
+  $("li").filter(function() {
+    $(this).toggle(
+      $(this)
+        .text()
+        .toLowerCase()
+        .indexOf(value) > -1
+    );
+  });
+});
